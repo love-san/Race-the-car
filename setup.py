cf7a1328918f8e4d70b7d9a42e1cfa798312341e
love-san <anubhav0603@gmail.com>
2018-10-01 11:53:15 +0530

    update some finishes

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
+# cx_Freeze is imported to get executable data
 import cx_Freeze
 
 executables=[cx_Freeze.Executable("Race_the_car.py")]
-cx_Freeze.setup(
 
+
+cx_Freeze.setup(
 	name="RacetheCar",
 	options={"build_exe":{"packages":["pygame","sys","pygame.surfarray","numpy","pygame._numpysurfarray"],
 						  "include_files": ["red2.jpg","blue2.jpg","car_11.png","car_22.png"]}},
-						 
-	executables=executables		
-	)
+	executables=executables
+)
